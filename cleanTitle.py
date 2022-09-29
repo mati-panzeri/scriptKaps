325417f3a1055554dc7e7a4fcf134bdd9b77c46d
mati-panzeri <88509153+mati-panzeri@users.noreply.github.com>
2022-09-29 17:22:59 -0300

    Magic begins
    first script, it cleans the '\n' from titles

---
+++ cleanTitle.py
@@ -0,0 +1,17 @@
+#Importar la libreria pandas
+import pandas as pd
+
+#Importamos archivo csv
+df = pd.read_csv("Kaps_adv_published.csv", sep=',')
+
+
+#Leemos cada título, le sacamos los '\n' y guardamos el titulo editado
+for frame in range(len(df)):
+    singleFrame = df.loc[frame, 'title']
+    singleFrame = list(map(lambda l: l.rstrip('\n'), singleFrame))
+    finalString = "".join(singleFrame)
+    
+    df.loc[frame, 'title'] = finalString
+
+#Exportamos la data formateada a un nuevo csv
+df.to_csv('Kaps_adv_published.csv', index = False)
